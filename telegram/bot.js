6de29f33b2af32713036522115dc55343c82f1a0
art1shok13 <romaartishok@gmail.com>
2021-11-01 17:05:23 +0200

    01.11.2021 17:05

---
+++ telegram/bot.js
@@ -0,0 +1,149 @@
+const { Telegraf, session, Scenes: { WizardScene, Stage }, Markup } = require('telegraf')
+const {subjects, subjects_markup, amarktypes_markup, bot_token, schedule, week, dateParser} = require('./config')
+
+//===========================================================================================================================
+const aMarkSubject = Telegraf.on('callback_query', async ctx => {
+
+    ctx.session.aMarkScene = {subject: Number(ctx.update.callback_query.data)}
+
+    await ctx.reply('Mark:')
+
+    return ctx.wizard.next()
+})
+
+const aMarkMark = Telegraf.on('text', async ctx => {
+
+    ctx.session.aMarkScene.mark = Number(ctx.update.message.text)
+
+    await ctx.reply('Choose type of work:', Markup.inlineKeyboard(amarktypes_markup))
+
+    return ctx.wizard.next()
+})
+
+const aMarkType = Telegraf.on('callback_query', async ctx => {
+
+    ctx.session.aMarkScene.type = Number(ctx.update.callback_query.data)
+
+    await ctx.reply('Name the work:')
+
+    return ctx.wizard.next()
+})
+
+const aMarkName = Telegraf.on('text', async ctx => {
+
+    ctx.session.aMarkScene.name = String(ctx.update.message.text)
+
+    const markup=schedule[ctx.session.aMarkScene.subject].map(
+        (item)=>{
+            let row=[]
+            for(i=0;i!=4;i++){
+                const date = new Date(new Date().getFullYear(), new Date().getMonth(), new Date().getDate() - (1-new Date().getDay() +i*7))
+                row.push({text:`${week[date.getDay()]}| ${date.getMonth()+1}-${date.getDate()}`, callback_data:date})
+            }
+            return row
+        }
+    )
+    markup.push([{text:'Special Date', callback_data:'$'}])
+
+    await ctx.reply('Date:', Markup.inlineKeyboard(markup))
+    return ctx.wizard.next()
+})
+
+const aMarkDate = Telegraf.on('callback_query', async ctx => {
+    if(ctx.update.callback_query.data!='$'){
+        ctx.session.aMarkScene.date = dateParser(ctx.update.callback_query.data)
+        aMarkSql(ctx, ctx.session.aMarkScene)
+    }else{
+        await ctx.reply(`Write the date like this ${dateParser(new Date())}:`)
+        return ctx.wizard.next()
+    }
+
+})
+
+const aMarkSpecial = Telegraf.on('text', async ctx => {
+    ctx.session.aMarkScene.date = dateParser(ctx.update.message.text)
+    aMarkSql(ctx, ctx.session.aMarkScene)
+})
+
+const aMarkSql = async ( ctx, data ) => {
+    console.log(data)
+    await ctx.reply('Mark added')
+    return ctx.scene.leave()
+}
+
+const aMarkScene = new WizardScene('aMarkScene', aMarkSubject, aMarkMark, aMarkType, aMarkName, aMarkDate, aMarkSpecial)
+aMarkScene.enter(ctx => {
+    ctx.reply('Choose subject:', Markup.inlineKeyboard(subjects_markup))
+})
+//===========================================================================================================================
+//===========================================================================================================================
+const aTaskSubject = Telegraf.on('callback_query', async ctx => {
+    ctx.session.aTaskScene={subject: Number(ctx.update.callback_query.data)}
+    await ctx.reply('Write the task:')
+    return ctx.wizard.next()
+})
+
+const aTaskTask = Telegraf.on('text', async ctx => {
+    ctx.session.aTaskScene.task = String(ctx.update.message.text)
+
+    const markup=schedule[ctx.session.aTaskScene.subject].map(
+        (item)=>{
+            let row=[]
+            for(i=0;i!=4;i++){
+                const date = new Date(new Date().getFullYear(), new Date().getMonth(), new Date().getDate() + (1-new Date().getDay() +i*7))
+                row.push({text:`${week[date.getDay()]}| ${date.getMonth()+1}-${date.getDate()}`, callback_data:date})
+            }
+            return row
+        }
+    )
+    markup.push([{text:'Special Date', callback_data:'$'}])
+
+    await ctx.reply('Date:', Markup.inlineKeyboard(markup))
+    return ctx.wizard.next()
+})
+
+const aTaskDate = Telegraf.on('callback_query', async ctx => {
+    if(ctx.update.callback_query.data!='$'){
+        ctx.session.aTaskScene.date = dateParser(ctx.update.callback_query.data)
+        aTaskSql(ctx, ctx.session.aTaskScene)
+    }else{
+        await ctx.reply(`Write the date like this ${dateParser(new Date())}:`)
+        return ctx.wizard.next()
+    }
+})
+
+const aTaskSpecial= Telegraf.on('', async ctx => {
+    ctx.session.aTaskScene.date = dateParser(ctx.update.message.text)
+    aTaskSql(ctx, ctx.session.aTaskScene)
+    return ctx.wizard.next()
+})
+
+const aTaskSql = async (ctx,data) => {
+    console.log(data)
+    await ctx.reply('Task added')
+    return ctx.scene.leave()
+}
+
+const aTaskScene = new WizardScene('aTaskScene', aTaskSubject, aTaskTask, aTaskDate, aTaskSpecial)
+aTaskScene.enter(ctx => {
+    ctx.reply('Choose subject:', Markup.inlineKeyboard(subjects_markup))
+})
+
+
+
+const stage = new Stage([ aMarkScene, aTaskScene])
+
+const bot = new Telegraf(bot_token)
+bot.use(session(), stage.middleware())
+
+bot.command('add_mark', ctx => {
+    ctx.scene.enter('aMarkScene')
+})
+
+bot.command('add_task', ctx => {
+    ctx.scene.enter('aTaskScene')
+})
+
+bot.launch()
+process.once('SIGINT', () => bot.stop('SIGINT'))
+process.once('SIGTERM', () => bot.stop('SIGTERM'))
---
+++ telegram/config.js
@@ -0,0 +1,41 @@
+const bot_token='1752244986:AAF_Url00FI9HywoZXYdJIj8YVTLirq6fkY'
+
+const subjects = [
+    {id:0, subject_name:'Алгебра'},
+    {id:1, subject_name:'Геометрія'},
+    {id:2, subject_name:'Фізика'},
+]
+
+const subjects_markup = [
+    [{text:'Алгебра',callback_data:'0'}],
+    [{text:'Геометрія', callback_data:'1'}],
+    [{text:'Фізика', callback_data:'2'}],    
+]
+
+const amarktypes_markup = [
+    [{text:'Робота на уроці', callback_data:'0'}],
+    [{text:'контрольна робота', callback_data:'1'}],
+    [{text:'Домашня робота', callback_data:'2'}],
+]
+
+
+const week = [
+    'Нд',
+    'Пн',
+    'Вт',
+    'Ср',
+    'Чт',
+    'Пт',
+    'Сб',
+]
+const schedule = [
+    [1],
+    [2, 3],
+    [4]
+]
+
+const dateParser = (date)=>{
+    return `${new Date(date).getDate()}-${new Date(date).getMonth()+1}-${new Date(date).getFullYear()}`
+}
+
+module.exports = {bot_token, subjects, subjects_markup, amarktypes_markup, schedule, week, dateParser}

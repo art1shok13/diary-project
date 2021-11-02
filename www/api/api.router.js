b7fe66c0a77bd54eb3460463f8c1e44dab1864e2
art1shok13 <romaartishok@gmail.com>
2021-11-02 18:37:58 +0200

    02.11.2021 18:37

--- www/api/api.router.js
+++ www/api/api.router.js
@@ -36,7 +36,7 @@ api.post('/marks', (req, res)=>{
     req.body.params = JSON.parse(req.body.params)
     connection.query(`SELECT * FROM marks`, async (err, result, fields)=>{
         let filtered=result
-        filtered.map((item)=>{
+        await filtered.map((item)=>{
             item.date_from=new Date(item.date_from).getMonth()
             return item
         })
@@ -56,7 +56,29 @@ api.post('/marks', (req, res)=>{
 
 // /api/tasks
 api.post('/tasks', (req, res)=>{
-    
+    req.body.params = JSON.parse(req.body.params)
+    connection.query('SELECT * FROM `tasks` ORDER BY `tasks`.`date_to` DESC', async (err, result, fields)=>{
+        let filtered=result
+        await Object.entries(req.body.params).forEach(([param_id])=>{
+            filtered = filtered.filter( (item) => {
+                for(parameter of req.body.params[param_id]){
+                    if(item[param_id]==parameter){
+                        return true
+                    }
+                }
+                return false  
+            })
+        })
+        await filtered.map((item)=>{
+            if(new Date().getTime()>=new Date(item.date_to).getTime()){ 
+                item.fired=true
+                return item
+            }else{
+                return item
+            }
+        })
+        res.status(200).send(filtered)
+    })
 })
 
 module.exports = {api}
